6d5092fb4256335af6a6a4a0e63f00c4b53a8324
Deyvison <deyvison.melo@dce.ufpb.br>
2015-02-09 11:30:50 -0300

    criando a classe supervisor

---
+++ src/Supervisor.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Supervisor {
+	
+	
+	private String nome;
+	private List<String> telefones;
+	private String matricula;
+	
+	public Supervisor(){
+		
+		this("Sem nome",new LinkedList<String>(),"Sem matricula");
+	}
+	
+	public Supervisor(String nome,List<String>telefones,String matricula){
+		
+		this.nome = nome;
+		this.telefones = telefones;
+		this.matricula = matricula;
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	
+}
+
